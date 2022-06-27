bb963ae4032bcbc4e10fa7c112161b05c278bd1e
braulioportela79 <braulioportela@outlook.com>
2022-06-27 20:55:00 -0300

    Exercício Finalizado

--- curso.js
+++ curso.js
@@ -1,11 +1,3 @@
-/*
-Crie um método para o objeto curso que receba um aluno (como parâmetro) e retorne true se ele aprovou no curso ou false em caso de reprovação. Para ser aprovado, o aluno tem que ter uma média igual ou acima da nota de aprovação e ter menos faltas que faltas máximas. Se tiver a mesma quantidade, tem que estar 10% acima da nota de aprovação.
-
-Crie um método para o objeto curso que percorra a lista de estudantes e retorne um array de booleanos com os resultados se os alunos aprovaram ou não.
-
-Importe o módulo estudantes.js, que contém uma lista de alunos, dentro do arquivo que contém o objeto curso. Substitua o conteúdo da propriedade lista de estudantes, pela lista de estudantes do arquivo estudantes.js e garanta que sigam funcionando todos os métodos corretamente. (revisar que o arquivo tenha todos os métodos corretamente).
-*/
-
 let Aluno = require('./estudantes');
 
 let Curso = {
